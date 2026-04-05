1a12797936172c910d9fb6a0f0cd102879de5faf
NenJacp <nenjacp@email.com>
2026-04-05 01:17:03 -0600

    Add csharp-language-server to mason.nvim

--- configs/nvim/lua/plugins/mason.lua
+++ configs/nvim/lua/plugins/mason.lua
@@ -5,6 +5,7 @@ return {
       ensure_installed = {
         -- LSP Servers
         "svelte-language-server",
+        "csharp-language-server",
         "typescript-language-server",
         "html-lsp",
         "css-lsp",
